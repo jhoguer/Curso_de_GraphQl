40af3666f4590c5c1c0e7411e68df0f0486b31aa
Jhon L Guerrero <jhoguer@gmail.com>
2020-04-09 19:53:47 -0500

    Pasando parametros a las queries

--- platzi-dir/lib/resolvers.js
+++ platzi-dir/lib/resolvers.js
@@ -9,14 +9,14 @@ const courses = [
     topic: 'programacion'
   },
   {
-    _id: 'anyid',
+    _id: 'anyid2',
     title: 'Mi titulo 2',
     teacher: 'Mi profesor',
     description: 'Una descripcion',
     topic: 'programacion'
   },
   {
-    _id: 'anyid',
+    _id: 'anyid3',
     title: 'Mi titulo 3',
     teacher: 'Mi profesor',
     description: 'Una descripcion',
@@ -28,6 +28,10 @@ module.exports = {
   Query: {
     getCourses: () => {
       return courses
+    },
+    getCourse: (root, args) => {
+      const course = courses.filter(course => course._id === args.id)
+      return course.pop()
     }
   }
 }
