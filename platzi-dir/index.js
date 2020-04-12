3a216a7a0c224afbf6806eb76bde07a29b5809ec
Jhon L Guerrero <jhoguer@gmail.com>
2020-04-12 10:46:37 -0500

    Agregando el frontend simple

--- platzi-dir/index.js
+++ platzi-dir/index.js
@@ -3,6 +3,7 @@
 require('dotenv').config()
 const { makeExecutableSchema } = require('graphql-tools')
 const express = require('express')
+const cors = require('cors')
 const gqlMiddleware = require('express-graphql')
 const { readFileSync } = require('fs')
 const { join } = require('path')
@@ -11,6 +12,7 @@ const resolvers = require('./lib/resolvers')
 
 const app = express()
 const port = process.env.port || 3001
+const isDev = process.env.NODE_ENV !== 'production'
 
 // definiendo el esquema inicial
 const typeDefs = readFileSync(
@@ -19,11 +21,12 @@ const typeDefs = readFileSync(
 )
 const schema = makeExecutableSchema( { typeDefs, resolvers} )
 
+app.use(cors())
 
 app.use('/api', gqlMiddleware({
   schema: schema,
   rootValue: resolvers,
-  graphiql: true
+  graphiql: isDev
 }))
 
 app.listen(port, () => {
